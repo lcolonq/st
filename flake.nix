e982b1d75eda54ab6b3051675fb21d32b3731bb5
LLLL Colonq <llll@colonq>
2025-01-31 00:08:09 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,25 @@
 {
-  description = "st dependencies";
-
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-
   outputs = {self, nixpkgs}:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      shell = pkgs.mkShell {
+      st = pkgs.stdenv.mkDerivation {
+        name = "st";
+        src = ./.;
+        outputs = ["out" "terminfo"];
         nativeBuildInputs = pkgs.st.nativeBuildInputs;
         buildInputs = pkgs.st.buildInputs;
+        installPhase = ''
+          export TERMINFO=$terminfo/share/terminfo
+          mkdir -p $TERMINFO $out/nix-support
+          echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
+          make install PREFIX=$out 
+        '';
       };
     in {
-      defaultPackage.x86_64-linux = shell;
       packages.x86_64-linux = {
-        inherit
-          shell;
+        inherit st;
+        default = st;
       };
     };
 }
